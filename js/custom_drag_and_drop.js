cb1bb50185948f9474f8edc48967b1eca0c82f37
rlnB2022 <beyer.richard73@gmail.com>
2023-12-03 18:37:27 -0600

    Can now hide extra innings

--- js/custom_drag_and_drop.js
+++ js/custom_drag_and_drop.js
@@ -4,20 +4,35 @@ const setupListeners = () => {
 		localStorage.setItem("innings", e.target.value);
 		showScoreboardInnings(e.target.value);
 	});
+
+	// show Extra Innings
+	const checkboxShowExtraInnings =
+		document.getElementById("extraInningsSwitch");
+
+	checkboxShowExtraInnings.addEventListener("change", function () {
+		if (this.checked) {
+			localStorage.setItem("extraInnings", "show");
+		} else {
+			localStorage.setItem("extraInnings", "hide");
+		}
+
+		showScoreboardInnings(localStorage.getItem("innings"));
+	});
 };
 
 const setupScoreSheet = () => {
 	// get data from localStorage or set to 9 if not found
 	let scoreboard_innings = localStorage.getItem("innings") || 9;
 
-	// if (scoreboard_innings === null) {
-	// 	localStorage.setItem("innings", 9);
-	// 	scoreboard_innings = 9;
-	// }
-
 	// set dropdown to number of innings
 	const selectInnings = document.getElementById("select-innings");
 	selectInnings.value = scoreboard_innings;
+
+	// show Extra Innings
+	let extraInnings = localStorage.getItem("extraInnings");
+	const checkboxShowExtraInnings =
+		document.getElementById("extraInningsSwitch");
+	checkboxShowExtraInnings.checked = extraInnings;
 
 	// show total number of innings
 	showScoreboardInnings(scoreboard_innings);
@@ -32,6 +47,7 @@ const showScoreboardInnings = (innings) => {
 	const scoreboardHomeInningElements = document.querySelectorAll(
 		".scoreboard-home-inning"
 	);
+	const scoreboardExtraInnings = document.querySelectorAll(".extra-innings");
 	scoreboardInningElements.forEach((el, index) => {
 		if (index + 1 + 9 > innings) {
 			el.classList.add("d-none");
@@ -51,6 +67,15 @@ const showScoreboardInnings = (innings) => {
 			el.classList.add("d-none");
 		} else {
 			el.classList.remove("d-none");
+		}
+	});
+	const showExtraInnings = localStorage.getItem("extraInnings");
+
+	scoreboardExtraInnings.forEach((el) => {
+		if (showExtraInnings === "show") {
+			el.classList.remove("d-none");
+		} else {
+			el.classList.add("d-none");
 		}
 	});
 
